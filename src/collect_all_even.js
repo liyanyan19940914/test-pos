958fb0e6f03ec08325c9b990fd9934d4af201c27
liyanyan19940914 <921258351@qq.com>
2016-05-17 14:53:53 +0800

    first commit

---
+++ src/collect_all_even.js
@@ -0,0 +1,20 @@
+
+function collect_same_elements(collection_a, object_b) {
+    var sameArray=[];
+    collection_a.forEach(function (element){
+        if(isExist(element.key,object_b.value)){
+            sameArray.push(element.key);
+        }
+    });
+    return sameArray;
+}
+
+function isExist(element,collection){
+    for(var i=0;i<collection.length;i++){
+        if(collection[i]===element){
+            return true;
+        }
+    }
+    return false;
+}
+module.exports = collect_same_elements;
